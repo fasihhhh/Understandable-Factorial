84f63fae9b2327b2ab4fb1d0893d73882400825f
fasihhhh <fasihrehman151@gmail.com>
2025-01-25 23:52:55 +0500

    No Negtive Entry

--- main.java
+++ main.java
@@ -2,12 +2,18 @@ import java.util.*;
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number");
+        System.out.println("Enter a Positive Integer Number");
+        int check;
         int n =sc.nextInt();
         long product =1;
         for(int i = 1; i <= n;  i ++){
             product = product * i; 
         }
+        if(n>0){
         System.out.println( n + "! = " + product);
+        }
+        else{
+            System.out.println("No Factorial for Negative Number");
+        }
     }
 }
