5b15de8b1b4f802a7b70588fa8239864de35e6e8
fasihhhh <fasihrehman151@gmail.com>
2025-01-25 22:53:47 +0500

    Changed Datatype of 'product' varible from int to long

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number");
         int n =sc.nextInt();
-        int product =1;
+        long product =1;
         for(int i = 1; i <= n;  i ++){
             product = product * i; 
         }
